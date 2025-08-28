e87496538229004f817db6b996b98b0c801af370
Ubuntu <ubuntu@client-instance-24qau9no.0kemv2bt5sbeffft2yy22rtbmf.dx.internal.cloudapp.net>
2025-08-28 21:57:12 +0000

    Test: dq_full

--- scripts/zeroshot/bbbp.sh
+++ scripts/zeroshot/bbbp.sh
@@ -7,7 +7,8 @@ export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
 python ${BASE_DIR}/zeroshot/inference.py \
     --pretrained_model_name_or_path unsloth/Llama-3.1-8B-Instruct \
     --tokenizer_path DongkiKim/Mol-Llama-3.1-8B-Instruct \
-    --data_dir $DATA_DIR \
+    --data_dir ${DATA_DIR} \
     --task_name bbbp \
-    --qformer_path ${BASE_DIR}/checkpoints/stage2_v2/epoch=01.ckpt \
-    --prompt_type default
+    --qformer_path ${BASE_DIR}/checkpoints/stage2/stage2_dqformer_full/last.ckpt \
+    --prompt_type default \
+    --use_dq_encoder
--- scripts/zeroshot/pampa.sh
+++ scripts/zeroshot/pampa.sh
@@ -7,8 +7,8 @@ export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
 python ${BASE_DIR}/zeroshot/inference.py \
     --pretrained_model_name_or_path unsloth/Llama-3.1-8B-Instruct \
     --tokenizer_path DongkiKim/Mol-Llama-3.1-8B-Instruct \
-    --data_dir $DATA_DIR \
+    --data_dir ${DATA_DIR} \
     --task_name pampa \
-    --qformer_path ${BASE_DIR}/checkpoints/stage2_v2.1/epoch=01.ckpt \
+    --qformer_path ${BASE_DIR}/checkpoints/stage2/stage2_dqformer_full/last.ckpt \
     --prompt_type default \
     --use_dq_encoder
