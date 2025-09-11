2da2501c8147fde19bf7813f994ea125079861b1
zjing29 <zjing29@biodatsci.uwo.ca>
2025-09-11 14:36:15 -0400

    Update: single card llm baselines

--- scripts/MoleculeQA/llm_baselines_mistral.sh
+++ scripts/MoleculeQA/llm_baselines_mistral.sh
@@ -3,7 +3,7 @@
 : "${DATA_DIR:?Environment variable DATA_DIR not set}"
 
 export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 
 python ${BASE_DIR}/evaluation/molecule_gen.py \
     --train_config_path ${BASE_DIR}/configs/moleculeqa/llm_baseline/train_config_mistral.yaml \
