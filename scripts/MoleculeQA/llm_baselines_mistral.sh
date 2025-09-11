ba01617792a1f39810616f67ba10e5f1269525b0
zjing29 <zjing29@biodatsci.uwo.ca>
2025-09-11 12:12:33 -0400

    Update: llm_baseline config change

--- scripts/MoleculeQA/llm_baselines_mistral.sh
+++ scripts/MoleculeQA/llm_baselines_mistral.sh
@@ -3,8 +3,8 @@
 : "${DATA_DIR:?Environment variable DATA_DIR not set}"
 
 export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python ${BASE_DIR}/evaluation/molecule_gen.py \
-    --train_config_path ${BASE_DIR}/configs/moleculeqa/llm_baseline/train_config.yaml \
+    --train_config_path ${BASE_DIR}/configs/moleculeqa/llm_baseline/train_config_mistral.yaml \
     --data_config_path ${BASE_DIR}/configs/moleculeqa/llm_baseline/data_config.yaml
---
+++ scripts/MoleculeQA/llm_baselines_qwen.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+: "${BASE_DIR:?Environment variable BASE_DIR not set}"
+: "${DATA_DIR:?Environment variable DATA_DIR not set}"
+
+export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+python ${BASE_DIR}/evaluation/molecule_gen.py \
+    --train_config_path ${BASE_DIR}/configs/moleculeqa/llm_baseline/train_config_qwen3.yaml \
+    --data_config_path ${BASE_DIR}/configs/moleculeqa/llm_baseline/data_config.yaml
