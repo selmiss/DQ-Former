f4233bbb9d802d8a34fd581600c0163959146a01
selmiss <zihaoj24@gmail.com>
2025-09-14 17:12:58 -0400

    Larger Bride prepared for training

---
+++ scripts/training/stage_1_dqw2d_Large.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+: "${BASE_DIR:?Environment variable BASE_DIR not set}"
+: "${DATA_DIR:?Environment variable DATA_DIR not set}"
+
+export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
+export CUDA_VISIBLE_DEVICES=0
+
+python ${BASE_DIR}/stage1.py \
+    --train_config_path ${BASE_DIR}/configs/stage1_dqw2d/train_config_large.yaml \
+    --data_config_path ${BASE_DIR}/configs/stage1_dqw2d/data_config.yaml
--- utils/tmp_clean.sh
+++ utils/tmp_clean.sh
@@ -1,11 +1,11 @@
 python ./utils/preprocess_smiles.py \
-  --input data/Molecule-oriented_Instructions/mol_gen_full/train.json \
-  --output data/Molecule-oriented_Instructions/mol_gen_full/train_clean.json \
-  --log data/Molecule-oriented_Instructions/mol_gen_full/train_invalid.log \
+  --input data/Molecule-oriented_Instructions/property_prediction_full/train.json \
+  --output data/Molecule-oriented_Instructions/property_prediction_full/train_clean.json \
+  --log data/Molecule-oriented_Instructions/property_prediction_full/train_invalid.log \
   --smiles-key smiles
 
 python ./utils/preprocess_smiles.py \
-  --input data/Molecule-oriented_Instructions/mol_gen_full/test.json \
-  --output data/Molecule-oriented_Instructions/mol_gen_full/test_clean.json \
-  --log data/Molecule-oriented_Instructions/mol_gen_full/test_invalid.log \
+  --input data/Molecule-oriented_Instructions/property_prediction_full/test.json \
+  --output data/Molecule-oriented_Instructions/property_prediction_full/test_clean.json \
+  --log data/Molecule-oriented_Instructions/property_prediction_full/test_invalid.log \
   --smiles-key smiles
