f17b38aaee9ab3962c5b90f16f3ce872eec7b95e
zjing29 <zjing29@biodatsci.uwo.ca>
2025-09-15 10:33:52 -0400

    sync

--- scripts/training/stage2_dqw2d_large.sh
+++ scripts/training/stage2_dqw2d_large.sh
@@ -3,7 +3,7 @@
 : "${DATA_DIR:?Environment variable DATA_DIR not set}"
 
 export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python ${BASE_DIR}/stage2.py \
     --train_config_path ${BASE_DIR}/configs/stage2_dqw2d/train_config_large.yaml \
